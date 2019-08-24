146c2b0394a54211e26ff3bb3a44b397c31a05b4
hamdyaea <hamdy.aea@gmail.com>
2019-08-24 12:15:37 +0200

    testing

--- netflix.py
+++ netflix.py
@@ -6,5 +6,25 @@ https://github.com/hamdyaea
 http://www.daylightlinux.ch
 '''
 
-from easygui import *
 
+
+import requests
+
+ApiKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" # Please subscribe to receive your api key
+
+def ListCountry():
+
+    url = "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi"
+
+    querystring = {"t":"lc","q":"available"}
+
+    headers = {
+        'x-rapidapi-host': "unogs-unogs-v1.p.rapidapi.com",  # Please subscribe to receive your api key
+        'x-rapidapi-key': ApiKey
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring,file=open("output.txt", "a"))
+
+    print(response.text)
+
+ListCountry()
---
+++ readme.md
@@ -0,0 +1,3 @@
+This software is not finish and I still working on it.
+
+Hamdy Abou El Anein
